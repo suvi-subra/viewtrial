1d71841c90d07cb1557eb703104d24a1c51a8092
suvi-subra <suvi.subra@gmail.com>
2020-09-28 15:07:33 +0200

    edited app.R

--- app.R
+++ app.R
@@ -4,17 +4,4 @@
 
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
-# Launch the ShinyApp (Do not remove this comment)
-# To deploy, run: rsconnect::deployApp()
-# Or use the blue button on top of this file
 
-pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-options( "golem.app.prod" = TRUE)
-viewTRiAL::run_app() # add parameters here (if any)
-# Launch the ShinyApp (Do not remove this comment)
-# To deploy, run: rsconnect::deployApp()
-# Or use the blue button on top of this file
-
-pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-options( "golem.app.prod" = TRUE)
-viewTRiAL::run_app() # add parameters here (if any)
